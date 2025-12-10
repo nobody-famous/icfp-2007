a18ce2f0831a09ada06f0d4a9143e9243d9c6e40
Rich Heller <rellehr@gmail.com>
2025-12-09 21:51:21 -0600

    Code cleanup

--- src/test/java/fuun/dna/MatcherTest.java
+++ src/test/java/fuun/dna/MatcherTest.java
@@ -1,7 +1,6 @@
 package fuun.dna;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
