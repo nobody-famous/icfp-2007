6f4d4aabe58b82b44d7150eedf76c4966179ec83
Rich Heller <rellehr@gmail.com>
2025-12-13 22:28:34 -0600

    Code cleanup

--- src/test/java/fuun/piecetable/PieceTableTest.java
+++ src/test/java/fuun/piecetable/PieceTableTest.java
@@ -1,6 +1,5 @@
 package fuun.piecetable;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
